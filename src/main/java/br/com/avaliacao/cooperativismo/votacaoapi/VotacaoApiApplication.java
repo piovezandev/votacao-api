d6a4d04bea0790dd1f9a5495cf2cace7c65cf0a8
Alan PIovezan <alan.log.piovezan@gmail.com>
2022-03-01 16:45:16 -0300

    Add repositorio

---
+++ src/main/java/br/com/avaliacao/cooperativismo/votacaoapi/VotacaoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.avaliacao.cooperativismo.votacaoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/avaliacao/cooperativismo/votacaoapi/VotacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.avaliacao.cooperativismo.votacaoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
