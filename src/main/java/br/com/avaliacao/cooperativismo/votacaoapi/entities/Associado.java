6de0fa6f8a43f6174094694516fad5bcded7dc3d
Alan PIovezan <alan.log.piovezan@gmail.com>
2022-03-01 22:52:45 -0300

    feature/versao-api: Ajuste atributos entidade - Add parametros para o atibuto CPF da entidade associado;

--- src/main/java/br/com/avaliacao/cooperativismo/votacaoapi/entities/Associado.java
+++ src/main/java/br/com/avaliacao/cooperativismo/votacaoapi/entities/Associado.java
@@ -1,5 +1,6 @@
 package br.com.avaliacao.cooperativismo.votacaoapi.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,6 +15,7 @@ public class Associado {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
+	@Column(name="cpf", nullable=false, length=11)
 	private String cpf;
 	
 	public Associado() {};
