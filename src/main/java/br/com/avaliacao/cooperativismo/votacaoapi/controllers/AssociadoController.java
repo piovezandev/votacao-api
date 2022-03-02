b44a4d463c92ce44f76df5abdf4f505d6b35d477
Alan PIovezan <alan.log.piovezan@gmail.com>
2022-03-01 22:55:18 -0300

    feature/versao-api:Criação camada de controllers - Criação da camada de controladores onde contem os metodos de cadastrar, listar todos e listar associados por cpf;

---
+++ src/main/java/br/com/avaliacao/cooperativismo/votacaoapi/controllers/AssociadoController.java
@@ -0,0 +1,43 @@
+package br.com.avaliacao.cooperativismo.votacaoapi.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.avaliacao.cooperativismo.votacaoapi.dto.AssociadoDTO;
+import br.com.avaliacao.cooperativismo.votacaoapi.entities.Associado;
+import br.com.avaliacao.cooperativismo.votacaoapi.services.AssociadoService;
+
+@RestController
+@RequestMapping(value = "/associado")
+public class AssociadoController {
+	
+	 Logger logger = LoggerFactory.getLogger(AssociadoController.class);
+	
+	@Autowired
+	private AssociadoService associadoService;
+
+	@PostMapping(value = "/cadastro")
+	public AssociadoDTO cadastrarAssociado(@RequestBody Associado associado){
+		return associadoService.salvarAssociado(associado);
+	}
+	
+	@GetMapping
+	public Page<AssociadoDTO> buscarTodosAssociados(Pageable pageable){
+		return associadoService.buscarTodos(pageable);
+	}
+	
+	@GetMapping("/{cpf}")
+	public AssociadoDTO buscaAssociadoPorCpf(@PathVariable String cpf, Pageable pageable) {
+		return associadoService.buscarAssociadoPorCPF(pageable, cpf);
+	}
+	
+}
