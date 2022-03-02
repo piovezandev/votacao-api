97354ce8d5f2fde3208b11035584718f5c759579
Alan PIovezan <alan.log.piovezan@gmail.com>
2022-03-01 22:56:29 -0300

    feature/versao-api:Criação repository persistencia de dados - Criação da interface repository para persistencia dos dados;

---
+++ src/main/java/br/com/avaliacao/cooperativismo/votacaoapi/repositories/AssociadoRepository.java
@@ -0,0 +1,9 @@
+package br.com.avaliacao.cooperativismo.votacaoapi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.avaliacao.cooperativismo.votacaoapi.entities.Associado;
+
+public interface AssociadoRepository extends JpaRepository<Associado, Long>{
+
+}
