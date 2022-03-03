3f04e9f29013422d38a3d37c75f50718a848049f
Alan PIovezan <alan.log.piovezan@gmail.com>
2022-03-03 08:40:00 -0300

    feature/versao-api:Add metodo persistencia dados; - add metodo para buscar o associado pelo CPF;

--- src/main/java/br/com/avaliacao/cooperativismo/votacaoapi/repositories/AssociadoRepository.java
+++ src/main/java/br/com/avaliacao/cooperativismo/votacaoapi/repositories/AssociadoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.avaliacao.cooperativismo.votacaoapi.entities.Associado;
 
 public interface AssociadoRepository extends JpaRepository<Associado, Long>{
-
+	Associado findByCpf(String cpf);
 }
