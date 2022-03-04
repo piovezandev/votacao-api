80320408e0bda1ed81973febcb21da2daa87ee34
Alan PIovezan <alan.log.piovezan@gmail.com>
2022-03-04 16:55:46 -0300

    feature/versao-api:Regra negocio computar votos - Add regras de negocio para poder computar os votos e abrir uma pauta; - Abertura de novas pautas parametrizadas por 1 min; - Add valdiação do tempo de 1 min via dataTime salva no banco; - Add soma de votos;

--- src/main/java/br/com/avaliacao/cooperativismo/votacaoapi/services/VotacaoService.java
+++ src/main/java/br/com/avaliacao/cooperativismo/votacaoapi/services/VotacaoService.java
@@ -1,5 +1,6 @@
 package br.com.avaliacao.cooperativismo.votacaoapi.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import br.com.avaliacao.cooperativismo.votacaoapi.entities.Pauta;
 import br.com.avaliacao.cooperativismo.votacaoapi.entities.Votacao;
 import br.com.avaliacao.cooperativismo.votacaoapi.enumeration.VotacaoEnum;
 import br.com.avaliacao.cooperativismo.votacaoapi.repositories.AssociadoRepository;
+import br.com.avaliacao.cooperativismo.votacaoapi.repositories.PautaRepository;
 import br.com.avaliacao.cooperativismo.votacaoapi.repositories.VotacaoRepository;
 
 @Service
@@ -27,6 +29,9 @@ public class VotacaoService {
 
 	@Autowired
 	private AssociadoRepository associadoRepository;
+	
+	@Autowired
+	private PautaRepository pautaRepository;
 
 	@Autowired
 	private PautaService pautaService;
@@ -35,7 +40,7 @@ public class VotacaoService {
 	public PautaDTO criaSessaoVotoPauta(VotacaoDTO votacaoDTO) {
 		boolean isDeveVotar = validaCpf(votacaoDTO.getCpf());
 
-		if (true) {
+		if (isDeveVotar) {
 
 			Associado associado = associadoRepository.findByCpf(votacaoDTO.getCpf());
 
@@ -47,7 +52,13 @@ public class VotacaoService {
 			}
 			
 			Pauta pauta = new Pauta();
-			pauta.setId(1L);
+				
+			getIdSession();
+			
+			if(validaData()) {	
+				
+			pauta.setDataSessao(LocalDateTime.now());			
+			pauta.setId(getIdSession());
 			
 			pautaService.abrirSessao(pauta);
 			
@@ -58,10 +69,24 @@ public class VotacaoService {
 			
 			votacao = votacaoRepository.saveAndFlush(votacao);	
 			
-			Integer teste  = getSomaValores(pauta.getId());
+			}else {
+				pauta.setDataSessao(LocalDateTime.now());
+				
+				pautaService.abrirSessao(pauta);
+				
+				Votacao votacao = new Votacao();
+				votacao.setAssociado(associado);
+				votacao.setVoto(votacaoDTO.getVoto());
+				votacao.setPauta(pauta);
+				
+				votacao = votacaoRepository.saveAndFlush(votacao);	
+			}
 			
-			pauta.setCount(teste);
+			Integer resultado  = getSomaVotos(pauta.getId());
 			
+			pauta.setCount(resultado);
+			
+			pautaRepository.saveAndFlush(pauta);	
 			return new PautaDTO(pauta);
 			
 		}
@@ -72,7 +97,7 @@ public class VotacaoService {
 	private Boolean validaCpf(String cpf) {
 		RestTemplate restTemplate = new RestTemplate();
 
-		StatusDTO status = restTemplate.getForObject(String.format(VALIDA_CPF_API, cpf), StatusDTO.class);
+		StatusDTO status = restTemplate.getForObject(String.format(VALIDA_CPF_API, cpf.trim()), StatusDTO.class);
 
 		if (status.getStatus().equals(VotacaoEnum.ABLE_TO_VOTE.isDeveVotar)) {
 			return true;
@@ -81,16 +106,37 @@ public class VotacaoService {
 		}
 	}
 	
-	public Integer getSomaValores(Long id) {
+	private Integer getSomaVotos(Long id) {
 		List<Votacao> listaEntidade = votacaoRepository.findAll();	
-//		&& p.getId().getPauta().getId()== id
+
 		Long somaValores = listaEntidade
 				.stream()
-				.filter(p -> p.getVoto().equals("SIM") || p.getVoto().equals("sim") ).count();
+				.filter(p -> p.getVoto().equals("SIM") || p.getVoto().equals("sim") && p.getId().getPauta().getId()== id).count();
 		
 		Integer resultado = somaValores.intValue();
 		
 		return resultado;
 	}
 
+	private Boolean validaData() {
+		LocalDateTime dataHoraAtual = LocalDateTime.now();
+		
+		List<Pauta> listaEntidade = pautaRepository.findAll();	
+
+		boolean retorno = listaEntidade.stream().anyMatch(p -> dataHoraAtual.isBefore(p.getDataSessao().plusMinutes(1)));		
+		
+		return retorno;
+	}
+	
+	
+	private Long getIdSession() {
+		Long idSessao = pautaRepository.findId();			
+		
+		if (idSessao == null) {
+			return 1L;
+		}
+		
+		return idSessao;
+	}
+	
 }
