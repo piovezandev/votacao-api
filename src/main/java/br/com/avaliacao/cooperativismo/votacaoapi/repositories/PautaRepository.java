8314426583aae3ab0eb3876c5d6510180c885c38
Alan PIovezan <alan.log.piovezan@gmail.com>
2022-03-04 16:53:37 -0300

    feature/versao-api:Add query repository - Add native query para poder obter o ultimo id de pautas;

--- src/main/java/br/com/avaliacao/cooperativismo/votacaoapi/repositories/PautaRepository.java
+++ src/main/java/br/com/avaliacao/cooperativismo/votacaoapi/repositories/PautaRepository.java
@@ -1,9 +1,11 @@
 package br.com.avaliacao.cooperativismo.votacaoapi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.avaliacao.cooperativismo.votacaoapi.entities.Pauta;
 
 public interface PautaRepository extends JpaRepository<Pauta, Long>{
-
+	@Query(value = "SELECT ID FROM TB_PAUTA ORDER BY ID DESC LIMIT 1", nativeQuery = true)
+	Long findId();
 }
