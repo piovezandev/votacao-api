bde891d0b3886e382d2b12b5378fb1bac2d0ffd1
Alan PIovezan <alan.log.piovezan@gmail.com>
2022-03-05 20:44:04 -0300

    feature/versao-api:Add logs de rastreabilidade - Add logs das rotinas de cadstro de pauta

--- src/main/java/br/com/avaliacao/cooperativismo/votacaoapi/controllers/PautaController.java
+++ src/main/java/br/com/avaliacao/cooperativismo/votacaoapi/controllers/PautaController.java
@@ -1,5 +1,7 @@
 package br.com.avaliacao.cooperativismo.votacaoapi.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,22 +19,27 @@ import br.com.avaliacao.cooperativismo.votacaoapi.services.PautaService;
 @RestController
 @RequestMapping(value = "/pautas")
 public class PautaController {
+	
+	Logger logger = LoggerFactory.getLogger(PautaController.class);
 
 	@Autowired
 	private PautaService pautaService;
 	
 	@PostMapping(value = "/criar")
 	public Pauta cadastraPauta(@RequestBody Pauta pauta) {
+		logger.info("Acessou a controller de criação de Pauta");
 		return pautaService.abrirSessao(pauta);
 	}
 	
 	@GetMapping
 	public Page<PautaDTO> buscarPautas(Pageable pageable){
+		logger.info("Acessou a controller de busca de Pauta paginado");
 		return pautaService.buscarTodos(pageable);
 	}
 	
 	@GetMapping("/{id}")
 	public PautaDTO buscaPautaPorId(@PathVariable Long id) {
+		logger.info("Acessou a controller de buscar de Pauta por id");
 		return pautaService.buscarPautaPorCodigo(id);
 	}
 	
--- src/main/java/br/com/avaliacao/cooperativismo/votacaoapi/services/PautaService.java
+++ src/main/java/br/com/avaliacao/cooperativismo/votacaoapi/services/PautaService.java
@@ -2,11 +2,14 @@ package br.com.avaliacao.cooperativismo.votacaoapi.services;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import br.com.avaliacao.cooperativismo.votacaoapi.controllers.PautaController;
 import br.com.avaliacao.cooperativismo.votacaoapi.dto.PautaDTO;
 import br.com.avaliacao.cooperativismo.votacaoapi.entities.Pauta;
 import br.com.avaliacao.cooperativismo.votacaoapi.repositories.PautaRepository;
@@ -14,21 +17,29 @@ import br.com.avaliacao.cooperativismo.votacaoapi.repositories.PautaRepository;
 @Service
 public class PautaService {
 
+	Logger logger = LoggerFactory.getLogger(PautaService.class);
+
 	@Autowired
 	PautaRepository pautaRepository;
 	
 	public Pauta abrirSessao(Pauta pauta) {
+		logger.info("Abrindo nova sessao de pauta");
+		
 		pautaRepository.saveAndFlush(pauta);
 		return pauta;
 	}
 	
 	public Page<PautaDTO> buscarTodos(Pageable pageable) {
+		logger.info("Buscando pautas de forma Paginada");
+
 		Page<Pauta> listaEntidade = pautaRepository.findAll(pageable);
 		Page<PautaDTO> response = listaEntidade.map(n -> new PautaDTO(n));
 		return response;
 	}
 	
 	public PautaDTO buscarPautaPorCodigo(Long id) {
+		logger.info(String.format("Buscando pauta por codigo id: [%s]", id));
+
 		List<Pauta> listaEntidade = pautaRepository.findAll();		
 		
 		return listaEntidade
